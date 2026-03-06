3540dae3401e94a0ba30b062dec5492e9b561210
Alexander742-Trr <rathim482@gmail.com>
2026-03-06 23:17:56 +0530

    Add files via upload

---
+++ Python Assignments/Assignment1/Task2.py
@@ -0,0 +1,4 @@
+First_name= input("Enter your First name: ")
+Last_name= input("Enter your Last name: ")
+Full_name = First_name + " " + Last_name
+print(f"Hello,{Full_name} Welcome to Python Programming!")
